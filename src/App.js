9fada3becdf0e4442d0193c6362b02bc0d44b15f
mpark123 <matthew_h_park@brown.edu>
2022-11-10 22:24:11 -0500

    done

--- src/App.js
+++ src/App.js
@@ -8,21 +8,53 @@ bakeryData.forEach((item) => {
 });
 /* ############################################################## */
 
+function CC(props) {
+  return(
+    <>
+    <img src = {props.item.image}></img>
+    <br></br>
+    <button onClick={() => {props.upC(props.index); props.pushP(props.price + props.item.price)}}>Lemme add one</button>
+    <h4>{props.item.name}</h4>
+    <p>{props.item.price}</p>
+    <p>{props.item.description}</p>
+    </>
+  )
+}
+
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [p, pushP] = useState(0); // set price
+  const [c, pushC] = useState({});
+  const upC = (i) => {
+    let mario = c;
+    {mario[i] ? (
+      mario[i] += 1
+    ) : (
+      mario[i] = 1
+    )}
+  pushC({...mario});
+  }
+
 
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+          <CC upC = {upC} item = {item} index = {index} price = {p} pushP = {pushP}></CC>
       ))}
 
       <div>
+      <br></br>
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        {Object.keys(c).map((key) => (
+          <>
+          {bakeryData[key].name + " " + c[key]}
+          </>
+        ))}
+        <h4>Total</h4>
+        <>{p}</>
       </div>
     </div>
   );
